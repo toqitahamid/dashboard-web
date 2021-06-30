26bcdfe43c0876a51803783ac51f0bfc7e99c126
toqitahamid <toqitahmid@gmail.com>
2021-06-30 13:45:32 +0600

    changed private key

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,8 +1,12 @@
 import * as firebaseAdmin from "firebase-admin";
 
-const privateKey = process.env["PRIVATE_KEY"];
-const clientEmail = process.env["CLIENT_EMAIL"];
-const projectId = process.env["PROJECT_ID"];
+// const privateKey = process.env["PRIVATE_KEY"];
+// const clientEmail = process.env["CLIENT_EMAIL"];
+// const projectId = process.env["PROJECT_ID"];
+
+const privateKey = process.env.PRIVATE_KEY;
+const clientEmail = process.env.CLIENT_EMAIL;
+const projectId = process.env.PROJECT_ID;
 
 if (!privateKey || !clientEmail || !projectId) {
   console.log(
